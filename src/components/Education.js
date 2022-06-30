85785f41504bf8691190b3413ebff28e0d546b8b
Morgan Hamet <morganhamet@gmail.com>
2022-06-30 14:56:45 -0700

    Get input for Education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,16 +3,70 @@ import React, { Component } from "react";
 class Education extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            school: "",
+            major: "",
+            dates: "",
+        }
     }
+
+    handleChangeSchool = (e) => {
+        this.setState({
+            school: e.target.value,
+            major: this.state.major,
+            dates: this.state.dates,
+        });
+    }
+
+    handleChangeMajor = (e) => {
+        this.setState({
+            school: this.state.school,
+            major: e.target.value,
+            dates: this.state.dates,
+        });
+    }
+
+    handleChangeDates = (e) => {
+        this.setState({
+            school: this.state.school,
+            major: this.state.major,
+            dates: e.target.value,
+        });
+    }
+
+    presentInfo = (e) => {
+        e.preventDefault();
+        console.log(this.state.school);
+        console.log(this.state.major);
+        console.log(this.state.dates);
+    }
+
     render() {
         return (
             <div>
                 <label htmlFor="school">University</label>
-                <input type="text" id="school" />
-                <label htmlFor="major">Major</label>
-                <input type="text" id="major" />
+                <input 
+                    onChange={this.handleChangeSchool}
+                    value={this.state.school}
+                    type="text" 
+                    id="school" 
+                />
+                    <label htmlFor="major">Major</label>
+                <input 
+                    onChange={this.handleChangeMajor}
+                    value={this.state.major}
+                    type="text" 
+                    id="major" 
+                />
                 <label htmlFor="studyDates">Dates of study</label>
-                <input type="date" id="studyDates" />
+                <input 
+                    onChange={this.handleChangeDates}
+                    value={this.state.dates}
+                    type="date" 
+                    id="studyDates" 
+                />
+                <button onClick={this.presentInfo}>Submit</button>
+
             </div>
         );
     }
--- src/components/General.js
+++ src/components/General.js
@@ -4,41 +4,41 @@ class General extends Component {
     constructor() {
         super();
         this.state = {
-            userName: "",
-            userEmail: "",
-            userPhone: "",
+            name: "",
+            email: "",
+            phone: "",
         }
     }
 
     handleChangeName = (e) => {
         this.setState({
-            userName: e.target.value,
-            userEmail: this.state.userEmail,
-            userPhone: this.state.userPhone,
+            name: e.target.value,
+            email: this.state.email,
+            phone: this.state.phone,
         });
     }
 
     handleChangeEmail = (e) => {
         this.setState({
-            userName: this.state.userName,
-            userEmail: e.target.value,
-            userPhone: this.state.userPhone,
+            name: this.state.name,
+            email: e.target.value,
+            phone: this.state.phone,
         });
     }
 
     handleChangePhone = (e) => {
         this.setState({
-            userName: this.state.userName,
-            userEmail: this.state.userEmail,
-            userPhone: e.target.value,
+            name: this.state.name,
+            email: this.state.email,
+            phone: e.target.value,
         });
     }
 
     presentInfo = (e) => {
         e.preventDefault();
-        console.log(this.state.userName);
-        console.log(this.state.userEmail);
-        console.log(this.state.userPhone);
+        console.log(this.state.name);
+        console.log(this.state.email);
+        console.log(this.state.phone);
     }
 
     render() {
@@ -48,19 +48,19 @@ class General extends Component {
                 <label htmlFor="name">Name</label>
                 <input 
                 onChange={this.handleChangeName}
-                value={this.state.userName}
+                value={this.state.name}
                 type="text" 
                 id="name" />
                 <label htmlFor="email">Email</label>
                 <input 
                 onChange={this.handleChangeEmail}
-                value={this.state.userEmail}
+                value={this.state.email}
                 type="email" 
                 id="email" />
                 <label htmlFor="phone">Phone number</label>
                 <input 
                 onChange={this.handleChangePhone}
-                value={this.state.userPhone}
+                value={this.state.phone}
                 type="tel" 
                 id="phone" />
                 <button onClick={this.presentInfo}>Submit</button>
