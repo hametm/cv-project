9dfebf3a05ae3acd89da7e4a816baea7b578ae3d
Morgan Hamet <morganhamet@gmail.com>
2022-06-30 15:07:07 -0700

    Get input from Practical section

--- src/components/Practical.js
+++ src/components/Practical.js
@@ -3,18 +3,114 @@ import React, { Component } from "react";
 class Practical extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            company: "",
+            position: "",
+            responsibilities: "",
+            startDate: "",
+            endDate: "",
+        }
     }
+
+    handleChangeCompany = (e) => {
+        this.setState({
+            company: e.target.value,
+            position: this.state.position,
+            responsibilities: this.state.responsibilities,
+            startDate: this.state.startDate,
+            endDate: this.state.endDate,
+        });
+    }
+
+    handleChangePosition = (e) => {
+        this.setState({
+            company: this.state.company,
+            position: e.target.value,
+            responsibilities: this.state.responsibilities,
+            startDate: this.state.startDate,
+            endDate: this.state.endDate,
+        });
+    }
+
+    handleChangeResponsibilities = (e) => {
+        this.setState({
+            company: this.state.company,
+            position: e.target.value,
+            responsibilities: e.target.value,
+            startDate: this.state.startDate,
+            endDate: this.state.endDate,
+        });
+    }
+
+    handleChangeStartDate = (e) => {
+        this.setState({
+            company: this.state.company,
+            position: this.state.position,
+            responsibilities: this.state.responsibilities,
+            startDate: e.target.value,
+            endDate: this.state.endDate,
+        });
+    }
+
+
+    handleChangeEndDate = (e) => {
+        this.setState({
+            company: this.state.company,
+            position: this.state.position,
+            responsibilities: this.state.responsibilities,
+            startDate: this.state.startDate,
+            endDate: e.target.value
+        });
+    }
+
+    presentInfo = (e) => {
+        e.preventDefault();
+        console.log(this.state.company);
+        console.log(this.state.position);
+        console.log(this.state.responsibilities);
+        console.log(this.state.startDate);
+        console.log(this.state.endDate);
+    }
+
     render() {
         return (
             <div>
                 <label htmlFor="company">Company</label>
-                <input type="text" id="company" />
+                <input 
+                    onChange={this.handleChangeCompany}
+                    value={this.state.company}
+                    type="text" 
+                    id="company" 
+                />
                 <label htmlFor="position">Position</label>
-                <input type="text" id="position" />
-                <label htmlFor="company">Company</label>
-                <input type="text" id="company" />
-                <label htmlFor="companyDates">Dates worked</label>
-                <input type="date" id="companyDates" />
+                <input 
+                    onChange={this.handleChangePosition}
+                    value={this.state.position}
+                    type="text" 
+                    id="position" 
+                />
+                <label htmlFor="responsibilities">Responsibilities</label>
+                <input 
+                    onChange={this.handleChangeResponsibilities}
+                    value={this.state.responsibilities}
+                    type="text" 
+                    id="responsibilities" 
+                />
+                <label htmlFor="startDate">Start date</label>
+                <input
+                    onChange={this.handleChangeStartDate}
+                    value={this.state.startDate} 
+                    type="date" 
+                    id="startDate" 
+                />
+                <label htmlFor="endDate">End date</label>
+                <input 
+                    onChange={this.handleChangeEndDate}
+                    value={this.state.endDate}
+                    type="date" 
+                    id="endDate" 
+                />
+                <button onClick={this.presentInfo}>Submit</button>
             </div>
         );
     }
